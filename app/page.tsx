ea8bb7351d5d07222d776c54ca6549891ab28b72
Jit Debnath <jit.nathdeb@gmail.com>
2025-02-22 02:12:21 +0530

    feat: Implement like button functionality and update page layout for better user interaction

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,35 @@
+'use client';
 import { FaGithub } from 'react-icons/fa';
-import { SiGithubsponsors } from 'react-icons/si';
 import { TbCube3dSphere } from 'react-icons/tb';
 import ModelPage from '@/components/model-page';
+import isMobile from '@/hooks/isMobile';
+import LoveButton from '@/components/like-button';
+import { useEffect, useState } from 'react';
+
 export default function Page() {
+  const [liked, setLiked] = useState(false);
+
+  // This effect runs once on mount to initialize the 'liked' state from localStorage
+  useEffect(() => {
+    const likedStatus = localStorage.getItem('liked');
+    if (likedStatus === null) {
+      localStorage.setItem('liked', 'false');
+      setLiked(false);
+    } else {
+      setLiked(likedStatus === 'true');
+    }
+  }, []);
+
+  const handleClick = () => {
+    const newLikedStatus = !liked;
+    setLiked(newLikedStatus);
+    localStorage.setItem('liked', newLikedStatus.toString());
+  };
+
   return (
     <>
       <div>
-        {/* navbar */}
+        {/* Navbar */}
         <div className="h-20 max-w-full bg-slate-800 flex justify-between items-center px-8">
           <h1 className="text-xl sm:text-2xl md:text-3xl">3D Model Viewer</h1>
           <div className="flex space-x-6 text-2xl">
@@ -18,23 +41,25 @@ export default function Page() {
             >
               <FaGithub className="" />
             </a>
-            <a href="#" title="Go to GitHub Sponsors">
-              <SiGithubsponsors />
-            </a>
+            <div onClick={handleClick}>
+              <LoveButton liked={liked} />
+            </div>
           </div>
         </div>
-        {/* upload model */}
+        {/* Upload model */}
         <div className="flex flex-col items-center justify-center h-[80vh]">
           <div className="w-[80vw] sm:max-w-2xl p-2 sm:p-8 bg-gray-600 rounded-lg shadow-md">
-            <div className=" relative border-4 border-dashed border-white rounded-lg h-60 sm:h-96 flex flex-col justify-center items-center">
+            <div className="relative border-4 border-dashed border-white rounded-lg h-60 sm:h-96 flex flex-col justify-center items-center">
               <TbCube3dSphere className="scale-150 text-5xl" />
               <p className="text-gray-500 m-2 text-center absolute bottom-0">
-                Drag and drop a file or click to select
+                {isMobile()
+                  ? 'Tap to select'
+                  : 'Drag and drop a file or click to select'}
               </p>
             </div>
           </div>
         </div>
-        {/* gallery view of example models */}
+        {/* Gallery view of example models */}
         <div className="p-8">
           <ModelPage />
         </div>
